79fa64352c2303bd6738f61a09ccb22b617ea95e
developer <prabesh065@gmail.com>
2022-12-01 10:42:20 +0545

    create-splash

---
+++ medv/android/app/src/main/kotlin/com/example/medv/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.medv
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ medv/lib/splash.dart
@@ -0,0 +1,41 @@
+import 'package:flutter/material.dart';
+import 'package:medv/Home.dart';
+import 'package:medv/main.dart';
+
+class splash extends StatefulWidget {
+  const splash({super.key});
+
+  @override
+  State<splash> createState() => _splashState();
+}
+
+class _splashState extends State<splash> {
+  @override
+  void initState() {
+    super.initState();
+    _navigatetohome();
+  }
+
+  _navigatetohome() async {
+    await Future.delayed(Duration(milliseconds: 1500), () {});
+    Navigator.pushReplacement(
+        context,
+        MaterialPageRoute(
+            builder: (context) => MyHomePage(
+                  title: 'something',
+                )));
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        body: Center(
+      child: Container(
+        child: Text(
+          'splash Screen',
+          style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+        ),
+      ),
+    ));
+  }
+}
