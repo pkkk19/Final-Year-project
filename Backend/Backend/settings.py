622f16cb757614cd2c5b061297f2276795e2a7fa
developer <prabesh065@gmail.com>
2022-12-24 22:37:13 +0545

    removing-heruko

--- Backend/Backend/settings.py
+++ Backend/Backend/settings.py
@@ -11,7 +11,6 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
 from pathlib import Path
-import django_heroku
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -133,7 +132,3 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-
-STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
-
-django_heroku.settings(locals())
