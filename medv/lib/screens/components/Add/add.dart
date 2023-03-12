91b8b61ae4a83de57ce96e8af9959fdab0edd5a7
developer <prabesh065@gmail.com>
2023-03-12 11:20:57 +0545

    add-page-update

---
+++ medv/lib/screens/components/Add/add.dart
@@ -0,0 +1,108 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/src/widgets/container.dart';
+import 'package:flutter/src/widgets/framework.dart';
+import 'package:medv/screens/components/Add/custom_title.dart';
+
+class Add extends StatefulWidget {
+  const Add({super.key});
+
+  @override
+  State<Add> createState() => _AddState();
+}
+
+class _AddState extends State<Add> {
+  bool darkMode = false;
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: darkMode ? Colors.grey[850] : Colors.grey[300],
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Spacer(),
+            CustomTitle(text: "Add"),
+            Spacer(),
+            Container(
+              width: 340,
+              height: 270,
+              child: Icon(Icons.file_copy,
+                  size: 80, color: darkMode ? Colors.white : Colors.black),
+              decoration: BoxDecoration(
+                  color: darkMode ? Colors.grey[850] : Colors.grey[300],
+                  borderRadius: BorderRadius.all(Radius.circular(50)),
+                  boxShadow: [
+                    BoxShadow(
+                        color: darkMode ? Colors.black54 : Colors.grey,
+                        offset: Offset(4.0, 4.0),
+                        blurRadius: 15.0,
+                        spreadRadius: 1.0),
+                    BoxShadow(
+                        color: darkMode ? Colors.grey : Colors.white,
+                        offset: Offset(-4.0, -4.0),
+                        blurRadius: 15.0,
+                        spreadRadius: 1.0),
+                  ]),
+            ),
+            Spacer(),
+            Container(
+              width: 340,
+              height: 270,
+              child: Icon(Icons.medication_liquid_outlined,
+                  size: 80, color: darkMode ? Colors.white : Colors.black),
+              decoration: BoxDecoration(
+                  color: darkMode ? Colors.grey[850] : Colors.grey[300],
+                  borderRadius: BorderRadius.all(Radius.circular(50)),
+                  boxShadow: [
+                    BoxShadow(
+                        color: darkMode ? Colors.black54 : Colors.grey,
+                        offset: Offset(4.0, 4.0),
+                        blurRadius: 15.0,
+                        spreadRadius: 1.0),
+                    BoxShadow(
+                        color: darkMode ? Colors.grey : Colors.white,
+                        offset: Offset(-4.0, -4.0),
+                        blurRadius: 15.0,
+                        spreadRadius: 1.0),
+                  ]),
+            ),
+            Spacer(),
+            // Row(
+            //   mainAxisAlignment: MainAxisAlignment.center,
+            //   children: <Widget>[
+            //     Padding(
+            //       padding: EdgeInsets.only(top: 50, right: 3),
+            //       child: ElevatedButton(
+            //         child: Text(
+            //           'Light',
+            //           style: TextStyle(color: Colors.black),
+            //         ),
+            //         onPressed: () {
+            //           setState(() {
+            //             darkMode = false;
+            //           });
+            //         },
+            //       ),
+            //     ),
+            //     Padding(
+            //       padding: EdgeInsets.only(top: 50, left: 3),
+            //       child: ElevatedButton(
+            //         child: Text(
+            //           'Dark',
+            //           style: TextStyle(color: Colors.white),
+            //         ),
+            //         onPressed: () {
+            //           setState(() {
+            //             darkMode = true;
+            //           });
+            //         },
+            //       ),
+            //     ),
+            //   ],
+            // ),
+          ],
+        ),
+      ),
+    );
+  }
+}
